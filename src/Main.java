cdd7b2e0ada269feadc7b6e42d7cf88f449b04b1
Coding Comets <Diegoalizo75@gmail.com>
2023-11-12 17:12:56 -0500

    Basic structure of car is ready

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    public static void main(String[] args) {
+        BasicStructure toyota = new BasicStructure();
+        toyota.color = "white";
+        toyota.model = "corolla";
+        toyota.power = "2000cc";
+    }
+}
